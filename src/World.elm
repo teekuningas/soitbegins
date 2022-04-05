7689c432f55b42fec7c64e6af22e9ff4ae38a8d7
Erkka Heinilä <erkkahe@gmail.com>
2022-04-05 21:03:20 +0300

    Update controller location and scaling

--- src/World.elm
+++ src/World.elm
@@ -69,8 +69,8 @@ fireUnif model =
                                                  Mat4.identity)) }
 
 
-controllerUnif : Model -> Uniforms
-controllerUnif model =
+controllerUnif : Model -> Float -> Float -> Float -> Uniforms
+controllerUnif model x y size =
   let xscale = ((toFloat model.canvasDimensions.width) /
                 (toFloat (Tuple.first viewportSize)))
       yscale = ((toFloat model.canvasDimensions.height) /
@@ -79,19 +79,16 @@ controllerUnif model =
   { rotation = Mat4.identity
 
   , location = 
-      Mat4.translate (vec3 7 -5 0) Mat4.identity
-
-  -- , perspective = 
-  --     Mat4.makePerspective 45 1 0.01 100
+      Mat4.translate (vec3 x y 0) Mat4.identity
 
   , perspective = 
-      Mat4.makeOrtho -10 10 -10 10 0.01 10
+      Mat4.makeOrtho -1 1 -1 1 0 10
 
   , camera = 
-      Mat4.makeLookAt (vec3 0 0 5) (vec3 0 0 0) (vec3 0 1 0)
+      Mat4.makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
 
   , scale =
-      Mat4.scale (vec3 (1/xscale) (1/yscale) 1) Mat4.identity
+      Mat4.scale (vec3 (size/xscale) (size/yscale) 1) Mat4.identity
 
   , shade = 0.5 } 
 
@@ -133,14 +130,14 @@ fireMesh =
 controllerMesh : Mesh Vertex
 controllerMesh =
   [ meshPositionMap 
-    (Vec3.add (vec3 0 0.2 0))
+    (Vec3.add (vec3 0 0.3 0))
     [ ( Vertex (vec3 0 0 1) (vec3 -1 0 0)
       , Vertex (vec3 0 1 0) (vec3 0 1 0)
       , Vertex (vec3 0 0 1) (vec3 1 0 0)
       )
     ]
   , meshPositionMap 
-    (Vec3.add (vec3 0 -0.2 0))
+    (Vec3.add (vec3 0 -0.3 0))
     [ ( Vertex (vec3 0 0 1) (vec3 1 0 0)
       , Vertex (vec3 1 0 0) (vec3 0 -1 0)
       , Vertex (vec3 0 0 1) (vec3 -1 0 0)
